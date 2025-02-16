d4c699958b7a057e7f97ac6a758dda690fbffaf4
Saikou BAH <infosaikoubah@gmail.com>
2025-02-16 23:01:51 +0100

    Ajout de la documentatin swagger sur les controllers.

--- src/main/java/com/ot/app/controller/PaymentController.java
+++ src/main/java/com/ot/app/controller/PaymentController.java
@@ -2,6 +2,14 @@ package com.ot.app.controller;
 
 import com.ot.app.model.Transaction;
 import com.ot.app.service.PaymentService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -9,6 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/payments")
+@Tag(name = "Transaction", description = "Api de paiement")
 public class PaymentController {
 
     private final PaymentService paymentService;
@@ -17,8 +26,10 @@ public class PaymentController {
         this.paymentService = paymentService;
     }
 
+    @Operation(operationId = "transactionId", description = "Permet de faire un paiement.")
+    @ApiResponses(value = {@ApiResponse(responseCode = "200", description = "Paiement effectuer", content = @Content(schema = @Schema(implementation = Transaction.class)))})
     @PostMapping
-    public Transaction makePayment(@RequestParam Long senderId, @RequestParam Long receiverId, @RequestParam double amount){
-        return paymentService.makePayment(senderId, receiverId, amount);
+    public ResponseEntity<Transaction> makePayment(@RequestParam Long senderId, @RequestParam Long receiverId, @RequestParam double amount) {
+        return new ResponseEntity<>(paymentService.makePayment(senderId, receiverId, amount), HttpStatus.ACCEPTED);
     }
 }
--- src/main/java/com/ot/app/controller/UserController.java
+++ src/main/java/com/ot/app/controller/UserController.java
@@ -3,6 +3,12 @@ package com.ot.app.controller;
 import com.ot.app.dto.UserDto;
 import com.ot.app.model.User;
 import com.ot.app.service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -10,6 +16,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/users")
+@Tag(name = "Utilisateurs", description = "Api de gestion des Uitilisateurs")
 public class UserController {
 
     private final UserService userService;
@@ -18,12 +25,16 @@ public class UserController {
         this.userService = userService;
     }
 
+    @Operation(operationId = "createUserId", description = "Créer un nouvel utilisateur")
+    @ApiResponses(value = {@ApiResponse(responseCode = "201", description = "Utilisateur creer", content = @Content(schema = @Schema(implementation = User.class)))})
     @PostMapping
     public ResponseEntity<User> createUser(@RequestBody @Valid UserDto userDto) {
         User user = userService.createUser(userDto);
         return new ResponseEntity<>(user, HttpStatus.CREATED);
     }
 
+    @Operation(operationId = "getBalanceId", description = "Récuperation du solde d'un utilisateur")
+    @ApiResponses(value = {@ApiResponse(responseCode = "200", description = "Information récupérer avec succès.", content = @Content(schema = @Schema(implementation = Double.class)))})
     @GetMapping("/{userId}/balances")
     public double getBalance(@PathVariable Long userId) {
         return userService.getBalance(userId);
