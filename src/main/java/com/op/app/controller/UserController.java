48e671f229345233385d2df56913c1634eee5109
Saikou BAH <infosaikoubah@gmail.com>
2025-12-29 18:07:53 +0100

    Refactorisation du code.

--- src/main/java/com/op/app/controller/UserController.java
+++ src/main/java/com/op/app/controller/UserController.java
@@ -2,7 +2,7 @@ package com.op.app.controller;
 
 import com.op.app.dto.UserDto;
 import com.op.app.model.User;
-import com.op.app.service.implementation.UserServiceImpl;
+import com.op.app.service.UserService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
@@ -26,20 +26,19 @@ import org.springframework.web.bind.annotation.RestController;
 @Tag(name = "Utilisateurs", description = "Api de gestion des Uitilisateurs")
 public class UserController {
 
-    private final UserServiceImpl userServiceImpl;
+    private final UserService userService;
 
     @Operation(operationId = "createUserId", description = "Créer un nouvel utilisateur")
     @ApiResponses(value = {@ApiResponse(responseCode = "201", description = "Utilisateur creer", content = @Content(schema = @Schema(implementation = User.class)))})
     @PostMapping
     public ResponseEntity<User> createUser(@RequestBody @Valid UserDto userDto) {
-        User user = userServiceImpl.createUser(userDto);
-        return new ResponseEntity<>(user, HttpStatus.CREATED);
+        return new ResponseEntity<>(userService.createUser(userDto), HttpStatus.CREATED);
     }
 
     @Operation(operationId = "getBalanceId", description = "Récuperation du solde d'un utilisateur")
     @ApiResponses(value = {@ApiResponse(responseCode = "200", description = "Information récupérer avec succès.", content = @Content(schema = @Schema(implementation = Double.class)))})
     @GetMapping("/{userId}/balances")
     public double getBalance(@PathVariable Long userId) {
-        return userServiceImpl.getBalance(userId);
+        return userService.getBalance(userId);
     }
 }
